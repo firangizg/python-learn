4188850b86312261a03674307424a67cc684d707
firangizg <65179277+firangizg@users.noreply.github.com>
2020-12-13 00:24:23 +0400

    Add madlib.py

---
+++ Madlib/madlib.py
@@ -0,0 +1,14 @@
+# define and ask for user input
+
+adj = input("Adjective: ")
+adj2 = input("Another adjective: ")
+bird = input("Type of a bird: ")
+room_name = input("Room in a house: ")
+verb = input("Verb in the past tense: ")
+relative = input("A relative's name: ")
+noun = input("Noun: ")
+liquid = input("Liquid: ")
+
+print(f"It was a {adj} November day. I woke up to the {adj2} smell of {bird} roasting in the {room_name} downstairs. \
+I {verb} down the stairs to see if I could help make the dinner. My mom said, \"See if {relative} needs a fresh {noun} \". \
+So I carried a tray of glasses full of {liquid} into the {room_name}.")
