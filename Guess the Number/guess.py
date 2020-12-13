55e110d45246dad762d3954f2fe2b63f7bbfa0f3
firangizg <65179277+firangizg@users.noreply.github.com>
2020-12-13 17:38:04 +0400

    Added files

---
+++ Guess the Number/guess.py
@@ -0,0 +1,13 @@
+import random
+
+roll = random.randint(1, 5)
+count = 0
+guess = 0
+
+while guess != roll:
+    count += 1
+    guess = input('Guess a number between 1 and 5: ')
+    if guess.isnumeric():
+        guess = int(guess)
+else:
+    print(f'You guessed it in {count} tries!')
